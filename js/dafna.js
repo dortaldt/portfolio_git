6ca88fb24b0d3cd3a6d4e0edf3789ad24a3061ba
Dor Tal <dortaldt@gmail.com>
2017-09-23 01:33:49 +0200

    V.01 all components

--- js/dafna.js
+++ js/dafna.js
@@ -1,2 +1,32 @@
-var answer = document.getElementById('answer')
-console.log(answer)
+var time = function(){
+	var d = new Date()
+	var s = d.getSeconds()
+	var h = d.getHours()
+	var m = d.getMinutes()
+	if(s<10) s = '0'+s
+	if(h<10) h = '0'+h
+	if(m<10) m = '0'+m
+	return (h + ':' + m + ':' + s)
+}
+
+var printUpdate = function() {
+	var current = time()
+	var oldTime = document.getElementById('update')
+	oldTime.innerHTML = current
+}
+
+var msgs = document.getElementsByClassName('loader');
+
+var animateText = function() {
+	msgs[0].className = 'loader show'
+	setTimeout(function(){
+		msgs[0].className = 'loader'
+		msgs[1].className = 'loader show'
+		setTimeout(function(){
+			msgs[1].className = 'loader'
+			printUpdate()
+		},2000);
+	},2000);
+}
+
+printUpdate()
